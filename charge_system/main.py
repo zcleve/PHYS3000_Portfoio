56b74821d90015c0557fc568d50f2aff4e47dd92
Zackary Cleveland <clevelandz@wit.edu>
2024-01-18 10:07:04 -0500

    Finished out current progress for worked prompts and cloned on machine 2. Pushing w/ IDE settings.

--- charge_system/main.py
+++ charge_system/main.py
@@ -1,8 +1,9 @@
 # Zackary Cleveland PHYS3000
-#
+# Attempted proficiencies:
+
 
 import netForcesChargeSystem as nfcs
 
-
-#nfcs.simulate_coulombs_law(<'PATH'>)
+# Run nfcs.simulate_coulombs_law('PATH') Ex: nfcs.simulate_coulombs_law('C:/Users/Zackary Cleveland/OneDrive -
+# Wentworth Institute of Technology/2024 Spring/progressReport2/read_files/charge_list2.txt')
 
