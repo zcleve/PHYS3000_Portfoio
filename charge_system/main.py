a3bf74074af50c16e552023c3b4ce5621514c868
zcleve <156138127+zcleve@users.noreply.github.com>
2024-01-22 23:22:47 -0500

    Delete charge_system directory

--- charge_system/main.py
+++
@@ -1,9 +0,0 @@
-# Zackary Cleveland PHYS3000
-# Attempted proficiencies: tbd
-
-import netForcesChargeSystem as nfcs
-
-PATH = r'C:\Users\Zack\PycharmProjects\COMP3000\read_files\charge_list.txt'
-
-nfcs.simulate_coulombs_law(PATH)
-plt.init_plot(PATH)
